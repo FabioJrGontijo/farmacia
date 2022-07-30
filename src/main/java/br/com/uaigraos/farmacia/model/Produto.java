6dcd42845785857b8a48b8af6c26230128d5bc0e
fabio gontijo <fabiojsabino@gmail.com>
2022-07-30 11:58:57 -0300

    inclusao id produto

--- src/main/java/br/com/uaigraos/farmacia/model/Produto.java
+++ src/main/java/br/com/uaigraos/farmacia/model/Produto.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Locale;
@@ -12,7 +15,9 @@ import java.util.Locale;
 @Setter
 @Getter
 public class Produto {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
     private String nome;
     private String principioAtivo;
     private LocalDateTime dataDoCadastro;
